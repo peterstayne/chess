d64f5943d40f112ce3ba4c1c8266098d2704b22d
Pete Milkman <peter_milkman@hotmail.com>
2012-03-26 23:35:44 -0400

    small knight refactor

--- js/chess.js
+++ js/chess.js
@@ -83,29 +83,37 @@ var perft = function(thisnode) {
 				}
 			}
 			if(tS === 8) { //white knight
-				if(curY > 2 && curX < 8 && tP[i-17] < 7) {
-					movelist.push([i, i-17]);
+				if(curY > 1) {
+					if(curY > 2) {
+						if(curX < 8 && tP[i-17] < 7) {
+							movelist.push([i, i-17]);
+						}
+						if(curX > 1 && tP[i-15] < 7) {
+							movelist.push([i, i-15]);	
+						}
+					}
+					if(curX < 7 && tP[i-6] < 7) {
+						movelist.push([i, i-6]);
+					}
+					if(curX > 2 && tP[i-10] < 7) {
+						movelist.push([i, i-10]);
+					}
 				}
-				if(curY > 2 && curX > 1 && tP[i-15] < 7) {
-					movelist.push([i, i-15]);	
-				}
-				if(curY > 1 && curX < 7 && tP[i-6] < 7) {
-					movelist.push([i, i-6]);
-				}
-				if(curY > 1 && curX > 2 && tP[i-10] < 7) {
-					movelist.push([i, i-10]);
-				}
-				if(curY < 7 && curX < 8 && tP[i+15] < 7) {
-					movelist.push([i, i+15])
-				}
-				if(curY < 7 && curX > 1 && tP[i+17] < 7) {
-					movelist.push([i, i+17])
-				}
-				if(curY < 8 && curX < 7 && tP[i+10] < 7) {
-					movelist.push([i, i+10]);
-				}
-				if(curY < 8 && curX > 2 && tP[i+6] < 7) {
-					movelist.push([i, i+6]);
+				if(curY < 8) {
+					if(curY < 7) {
+						if(curX < 8 && tP[i+15] < 7) {
+							movelist.push([i, i+15])
+						}
+						if(curX > 1 && tP[i+17] < 7) {
+							movelist.push([i, i+17])
+						}
+					}
+					if(curX < 7 && tP[i+10] < 7) {
+						movelist.push([i, i+10]);
+					}
+					if(curX > 2 && tP[i+6] < 7) {
+						movelist.push([i, i+6]);
+					}
 				}
 			}
 		} else {
